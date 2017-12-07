f6e3d1e1ee6e39974893266bde0f4fa711f4c74f
Kairs0 <arnaud.convers@gmail.com>
2017-12-07 23:26:44 +0100

    clean struct + git ignore

---
+++ traitements_linguistiques/code-arnaud/block.py
@@ -0,0 +1,21 @@
+import nltk
+
+
+class Block:
+
+    def __init__(self, type_block, content):
+        self.type_block = type_block
+        self.content = content
+        self.clean_content = ""
+        self.tokens = []
+
+    def clean_words(self):
+        self.clean_content = self.content.replace('.', ' ')\
+                .replace(',', ' ')\
+                .replace(':', ' ')\
+                .replace('(', ' ')\
+                .replace(')', ' ')\
+
+
+    def tokenize(self):
+        self.tokens = nltk.word_tokenize(self.clean_content)
---
+++ traitements_linguistiques/code-arnaud/collection.py
@@ -0,0 +1,25 @@
+from block import *
+
+
+class Collection:
+
+    def __init__(self, path_file, title):
+        self.title = title
+        self.path = path_file
+        self.blocks = []
+        self.tokens = []
+        with open(self.path) as f:
+            self.content = f.read()
+
+    def calc_blocks(self):
+        data = self.content.split("\n.")
+        for data_block in data:
+            if data_block[0] == 'T' or data_block[0] == 'W' or data_block[0] == 'K':
+                new_block = Block(data_block[0], data_block)
+                self.blocks.append(new_block)
+
+    def tokenize(self):
+        for block in self.blocks:
+            block.clean_words()
+            block.tokenize()
+            self.tokens += block.tokens
